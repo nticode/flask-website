55e2fd1eb89a05595276d0e9383883302eec067a
thekevie <itskevy.lidstrom@outlook.com>
2022-11-17 13:25:17 +0100

    upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+item = "LED"
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/api/<state>", methods = ['POST'])
+def on(state):
+    # {state} can be "on" or "off"
+    print(state)
+
+    return {"message": f"{item} has been turned {state}"}
+
+app.run('127.0.0.1', '1011', debug=True)
+print("Online")
